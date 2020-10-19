6fbb8e03fd066c0336dfa005f4f802810d53cbb1
Thomas A Johansen <thomasaj@gmail.com>
2020-10-19 11:05:51 +0200

    Adding custom colors

---
+++ stufs/Extensions/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  stufs
+//
+//  Created by Thomas Andre Johansen on 19/10/2020.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static let selectedTabColor = UIColor(named: "selectedTabColor")
+    static let nonselectedTabColor = UIColor(named: "nonselectedTabColor")
+    
+    func getAsImage(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            self.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+}
