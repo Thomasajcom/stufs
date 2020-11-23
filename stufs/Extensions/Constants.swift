3667fea16ef87a5a289a3c15a685cd4658f7da10
Thomas A Johansen <thomasaj@gmail.com>
2020-11-23 18:04:24 +0100

    Adding a place to keep constants.

---
+++ stufs/Extensions/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  stufs
+//
+//  Created by Thomas Andre Johansen on 23/11/2020.
+//
+
+import UIKit
+
+class Constants {
+    /// The offset to present the filtersheet from the bottom of the screen
+    static let filterSheetOffset: CGFloat = 70
+}
